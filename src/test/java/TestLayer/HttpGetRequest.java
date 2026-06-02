42cffe5481d25cab28124d829659eda67a0512a2
Taterao30 <tateraog30@gmail.com>
2026-06-03 03:01:47 +0530

    added basic Get request code

--- src/test/java/TestLayer/HttpGetRequest.java
+++ src/test/java/TestLayer/HttpGetRequest.java
@@ -1,8 +1,48 @@
 package TestLayer;
 
-public class HttpGetRequest {
-	
-	
-	
+import io.restassured.RestAssured;
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 
-}
+public class HttpGetRequest {
+
+
+		public static void main(String[] args) {
+			// step 1; Set the Base URI
+			RestAssured.baseURI = "http://localhost:3000/employees";
+
+			// step 2: get the Request specification object so we can select HTTP Request
+			RequestSpecification httpRequest = RestAssured.given();
+
+			// step 3: select the HTTP GET Request from RequestSpecification object
+			Response resp = httpRequest.get();
+
+			// step 4: capture status code from Response object
+			System.out.println(resp.getStatusCode());
+			System.out.println(resp.statusCode());
+
+			// step 5: capture status line from Response Object
+			System.out.println(resp.getStatusLine());
+			System.out.println(resp.statusLine());
+
+			// step 6: capture response time from Response Object
+			System.out.println(resp.getTime());
+			System.out.println(resp.time());
+
+			// step 7: capture respoonse body from Response object
+			System.out.println(resp.getBody().asString());
+			System.out.println(resp.body().asString());
+
+			// step 8: capture response headers from Response Object
+			Headers allheader = resp.getHeaders();
+
+			for (Header abc : allheader) {
+				System.out.println(abc.getName() + "  ::  " + abc.getValue());
+			}
+
+		}
+
+	}
+
